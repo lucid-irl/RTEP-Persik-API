f3a55be90f66146285fa26bef2ac761e9fbbfe31
lucid-irl <tranhuyhoang3103@gmail.com>
2021-10-03 17:52:57 +0700

    init project

---
+++ src/main/java/com/api/persik/PersikApplication.java
@@ -0,0 +1,13 @@
+package com.api.persik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersikApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PersikApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/api/persik/PersikApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.persik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersikApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
