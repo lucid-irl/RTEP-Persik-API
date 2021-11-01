d7d6927ad5946a6a6dcc787e5f8358b6d8905b35
truongaxin123 <truongaxin123@gmail.com>
2021-11-01 11:16:08 +0700

    init services

---
+++ src/main/java/com/api/persik/Services/UserService.java
@@ -0,0 +1,11 @@
+package com.api.persik.Services;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    public UserDetails loadUserById(Integer id){
+        return null;
+    }
+}
