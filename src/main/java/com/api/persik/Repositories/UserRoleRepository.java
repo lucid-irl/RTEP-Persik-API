70ea0566f95e97af58f972fca576d295217e9f00
truongaxin123 <truongaxin123@gmail.com>
2021-11-06 16:40:35 +0700

    add repositories

--- src/main/java/com/api/persik/Repositories/UserRoleRepository.java
+++
@@ -1,8 +0,0 @@
-package com.api.persik.Repositories;
-
-import com.api.persik.Models.Entities.UserRole;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRoleRepository extends JpaRepository<UserRole, Integer> {
-    UserRole getUserRoleById(Integer id);
-}
